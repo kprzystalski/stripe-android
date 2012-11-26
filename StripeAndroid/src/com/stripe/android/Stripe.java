c36475c7c5aa0decdf0dfa46af504a2d614e291b
Karol Przystalski <karol@codete.co>
2012-11-26 18:45:21 +0100

    deleted unused imports

--- StripeAndroid/src/com/stripe/android/Stripe.java
+++ StripeAndroid/src/com/stripe/android/Stripe.java
@@ -1,13 +1,5 @@
 package com.stripe.android;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import com.stripe.android.model.Charge;
-import com.stripe.android.utils.CVCNumberEditText;
-import com.stripe.android.utils.CardNumberEditText;
-import com.stripe.android.utils.MonthEditText;
-import com.stripe.android.utils.YearEditText;
 
 public abstract class Stripe {
 	public static final String API_BASE = "https://api.stripe.com";
